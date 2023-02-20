0c8b8c79d438b1cfc2911a713ab1aca51ada25a3
Bret Myers <bretamyers@gmail.com>
2023-02-20 11:49:49 -0500

    Create RunGen.sh

---
+++ main/RunGen.sh
@@ -0,0 +1,19 @@
+
+
+python -m venv .genvenv
+
+python -m pip install --upgrade pip 
+
+source .venv/bin/activate
+
+# curl -H "Authorization: token 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" -H "Accept: application/octet-stream" https://github.com/bretamyers/EventHub-Throughput-Generator/archive/master.zip -L -o EventHub-Throughput-Generator.zip
+
+# unzip -o EventHub-Throughput-Generator.zip -d EventHub-Throughput-Generator
+
+python -m pip install -r EventHub-Throughput-Generator/EventHub-Throughput-Generator-main/requirements.txt
+
+python EventHub-Throughput-Generator/EventHub-Throughput-Generator-main/main/BuildBatch.py
+
+rm -r EventHub-Throughput-Generator.zip
+
+rm -r .genvenv
